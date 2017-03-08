8a3af46b112a293474b3eb541bb198e8f130f640
sbstjn <mail@sbstjn.com>
2017-03-08 21:44:44 +0100

    Update syntax for eslint

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-'use strict';
-
 class Lawos {
   constructor(queueUrl, sqs) {
     this.maxMessages = 10;
@@ -7,17 +5,17 @@ class Lawos {
     this.sqs = sqs;
 
     this.handler = {
-      item: data => Promise.resolve(),
-      list: data => Promise.resolve()
+      item: () => Promise.resolve(),
+      list: () => Promise.resolve(),
     };
 
     this.metrics = {
       processed: 0,
-      iteration: 0
+      iteration: 0,
     };
 
     if (!this.queueUrl) {
-      throw new Error('Missing URL for SQS Queue')
+      throw new Error('Missing URL for SQS Queue');
     }
   }
 
@@ -25,8 +23,8 @@ class Lawos {
     return this.sqs.deleteMessage(
       {
         QueueUrl: this.queueUrl,
-        ReceiptHandle: id
-      }
+        ReceiptHandle: id,
+      },
     ).promise();
   }
 
@@ -34,18 +32,18 @@ class Lawos {
     return this.sqs.receiveMessage(
       {
         MaxNumberOfMessages: this.maxMessages,
-        MessageAttributeNames: [ 'All' ],
-        QueueUrl: this.queueUrl
-      }
+        MessageAttributeNames: ['All'],
+        QueueUrl: this.queueUrl,
+      },
     ).promise().then(
       data => {
         this.metrics.iteration += 1;
 
         return data;
-      }
+      },
     ).then(
-      list => list && list.Messages || this.quit()
-    )
+      list => list && list.Messages || this.quit(),
+    );
   }
 
   list(func) {
@@ -66,37 +64,37 @@ class Lawos {
         item => {
           this.metrics.processed += 1;
 
-          return this.handler.item(item)
-        }
-      )
+          return this.handler.item(item);
+        },
+      ),
     ).then(
-      data => list
+      () => list,
     ).then(
-      this.handler.list
+      this.handler.list,
     ).then(
       Promise.all(
         list.map(
-          item => this.delete(item.ReceiptHandle)
-        )
-      )
+          item => this.delete(item.ReceiptHandle),
+        ),
+      ),
     ).then(
-      data => list
+      () => list,
     );
   }
 
   quit() {
-    return Promise.resolve(this.metrics)
+    return Promise.resolve(this.metrics);
   }
 
   work(condition) {
     return condition().then(
       () => this.load().then(
-        list => this.process(list)
+        list => this.process(list),
       ).then(
-        () => this.work(condition)
-      )
+        () => this.work(condition),
+      ),
     ).catch(
-      () => this.quit()
+      () => this.quit(),
     );
   }
 }
